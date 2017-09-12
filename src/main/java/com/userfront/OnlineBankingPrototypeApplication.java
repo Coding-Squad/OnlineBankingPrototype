b848a4a669d3d89f1dd0fc588eb0a22bbf8f16c6
Coding-Squad <islammasum34@gmail.com>
2017-09-12 08:49:32 +0600

    Initial commit

---
+++ src/main/java/com/userfront/OnlineBankingPrototypeApplication.java
@@ -0,0 +1,12 @@
+package com.userfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBankingPrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBankingPrototypeApplication.class, args);
+	}
+}
